6785b5dc81913f13612774fa785f70315835b2e3
David <hajdarovicdavid@gmail.com>
2023-08-08 17:33:31 +0200

    minor migration fix

--- database/migrations/2023_08_07_065816_create_posts_table.php
+++ database/migrations/2023_08_07_065816_create_posts_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->unsignedBigInteger('user_id')->unsigned();
             $table->string('post_title');
             $table->longText('post_body');
             $table->timestamps();
+           
+        });
+
+        Schema::table('posts', function($table){
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
